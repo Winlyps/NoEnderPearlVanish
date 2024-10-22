dfb2336ee70cc082b72ac2c8eedc0ebc1a15af2e
Winlypse <nts3@o2.pl>
2024-10-22 17:40:12 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noEnderPearlVanish/NoEnderPearlVanish.kt
@@ -0,0 +1,14 @@
+package winlyps.noEnderPearlVanish
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoEnderPearlVanish : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
